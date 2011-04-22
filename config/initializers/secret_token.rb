b87f82d2101c2c108bc5bf304584ff4ecb611011
Blair Kutzman <bkutzman@bkubuntu.(none)>
2011-04-22 12:08:35 -0700

    Rails create repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '9357f3f77df21740a965d1c9363ac8d5bbf0bb1dd336bad19bed70d2c5ca37143daf53b96490b46c63d9fbe81ff7de2db9b6ac4eb164f0aa11e056eb56a796ed'
